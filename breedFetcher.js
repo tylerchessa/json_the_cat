50f3874cb418091aacd0c11b72f82ce632894491
tylerchessa <tylerchessa@gmail.com>
2023-02-17 00:24:53 +0000

    breedFetcher.js with a callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,25 @@
 const request = require('request');
 
-const cla = process.argv.splice(2);
-let typeOfCat = "";
-typeOfCat = cla;
-let link = "https://api.thecatapi.com/v1/breeds/search?q=";
-// let link = "https://tapi.com/v1/breeds/search?q="
-const body = request(link + typeOfCat, (error, response, body) => {
-  if (error) {
-    console.log("error ",);
-    return;
-  }
 
-  if (!body.includes(cla)) {
-    console.log("breed not found");
-    return;
-  }
+const fetchBreedDescription = (breedName, callback) => {
+  let link = "https://api.thecatapi.com/v1/breeds/search?q=";
+  request(link + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-  if (body) {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
+    if (!body.includes(breedName)) {
+      callback("breed not found", null);
+      return;
+    }
 
-  // console.log(data[0].description)
+    if (body) {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
 
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
